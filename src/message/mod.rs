d91ea86a470e02669c0b223a5fc6af06a0b4d696
JerryQ <1397200108@qq.com>
2023-09-18 00:37:23 +0800

    添加了`struct Message`，方法未定义/实现

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,1 +1,23 @@
 mod cq_code;
+
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Message {
+    pub messages: Vec<String>,
+}
+
+impl ToString for Message {
+    fn to_string(&self) -> String {
+        self.messages.join("")
+    }
+}
+
+impl FromStr for Message {
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
+}
