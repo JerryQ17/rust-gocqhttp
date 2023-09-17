d28a390da1b2f2823c16cf792c006286650f730a
JerryQ <1397200108@qq.com>
2023-09-17 13:32:55 +0800

    完成了`CQCode::from_string`方法

--- src/cq_code/mod.rs
+++ src/cq_code/mod.rs
@@ -1,25 +1,39 @@
-use serde::de::DeserializeOwned;
-use serde::Serialize;
+// use serde::de::DeserializeOwned;
+// use serde::Serialize;
 
-pub trait CQCode: Serialize + DeserializeOwned {
+pub trait CQCode: Sized {
+    fn escape(s: String) -> String {
+        s.replace('&', "&amp;")
+            .replace('[', "&#91;")
+            .replace(']', "&#93;")
+            .replace(',', "&#44;")
+    }
+
+    fn anti_escape(s: &str) -> String {
+        s.replace("&#44;", ",")
+            .replace("&#93;", "]")
+            .replace("&#91;", "[")
+            .replace("&amp;", "&")
+    }
+
     fn to_string(&self) -> String;
 
-    // fn from_string(s: String) -> Result<Self>;
+    fn from_string(s: String) -> crate::Result<Self>;
     //
     // fn to_json(&self) -> Result<String>;
     //
     // fn from_json(s: &str) -> Result<Self>;
 }
 
-#[cfg(test)]
 mod tests {
     use super::CQCode;
-    use serde::{Deserialize, Serialize};
+    use cq_code_derive::CQCode;
+    // use serde::{Deserialize, Serialize};
 
-    #[derive(Debug, Serialize, Deserialize, cq_code_derive::CQCode)]
+    #[derive(Debug, CQCode)]
     struct T {
-        a: Option<f64>,
-        b: Option<String>,
+        pub a: Option<f64>,
+        pub b: Option<String>,
     }
 
     #[test]
@@ -29,5 +43,12 @@ mod tests {
             b: Some("你好".to_string()),
         };
         assert_eq!(t.to_string().as_str(), "[CQ:T,a=1.5,b=你好]")
+    }
+
+    #[test]
+    fn test_from_string() {
+        let t: T = T::from_string("[CQ:T,a=1.5,b=你好]".to_string()).unwrap();
+        assert_eq!(t.a, Some(1.5));
+        assert_eq!(t.b, Some("你好".to_string()));
     }
 }
--- src/cq_code_derive/src/lib.rs
+++ src/cq_code_derive/src/lib.rs
@@ -1,18 +1,18 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, Ident, Type};
 
 #[proc_macro_derive(CQCode)]
-pub fn cq_code_derive(input: TokenStream) -> TokenStream {
+pub fn cq_code_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     impl_cq_code(&ast)
 }
 
-fn get_field(ast: &DeriveInput) -> Vec<&Ident> {
+fn mapping_input<T>(ast: &DeriveInput, f: impl FnMut(&Field) -> T) -> Vec<T> {
     let data = &ast.data;
     if let Data::Struct(s) = data {
         if let Fields::Named(n) = &s.fields {
-            Vec::from_iter(n.named.iter().map(|f| f.ident.as_ref().unwrap()))
+            Vec::from_iter(n.named.iter().map(f))
         } else {
             panic!("CQCode can only be implemented for structs with named fields")
         }
@@ -21,24 +21,81 @@ fn get_field(ast: &DeriveInput) -> Vec<&Ident> {
     }
 }
 
-fn impl_cq_code(ast: &DeriveInput) -> TokenStream {
-    let name = &ast.ident;
-    let fields = get_field(ast);
-    let body = quote! {
-        let mut result = format!("[CQ:{}", stringify!(#name));
-        #(
-            if let Some(ref #fields) = self.#fields {
-                result += &format!(",{}={}", stringify!(#fields), #fields);
+fn get_field(ast: &DeriveInput) -> Vec<Ident> {
+    mapping_input(ast, |f| f.ident.clone().unwrap())
+}
+
+fn get_type(ast: &DeriveInput) -> Vec<Type> {
+    mapping_input(ast, |f| f.ty.clone())
+}
+
+fn impl_to_string(name: &Ident, fields: &Vec<Ident>) -> TokenStream {
+    quote! {
+        fn to_string(&self) -> String {
+            let mut result = format!("[CQ:{}", stringify!(#name));
+            #(
+                if let Some(ref fields) = self.#fields {
+                    result += &format!(
+                        ",{}={}",
+                        stringify!(#fields),
+                        Self::escape(fields.to_string())
+                    );
+                }
+            )*
+            result += "]";
+            result
+        }
+    }
+}
+
+fn impl_from_string(name: &Ident, fields: &Vec<Ident>, ty: &Vec<Type>) -> TokenStream {
+    quote! {
+        fn from_string(s: String) -> crate::Result<Self> {
+            let re = regex::Regex::new(r"\[CQ:(?P<name>\w+)(?P<fields>(,\w+=[^,\]]+)*)\]").unwrap();
+            let caps = re.captures(&s).unwrap();
+            let name = caps.name("name").unwrap().as_str();
+            if name != stringify!(#name) {
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "CQCode类型不匹配"
+                )));
             }
-        )*
-        result += "]";
-        result
-    };
+            let fields = caps.name("fields").unwrap().as_str();
+            let mut result = #name {
+                #(
+                    #fields: None,
+                )*
+            };
+            let re = regex::Regex::new(r"(?P<field>\w+)=(?P<value>[^,\]]+)")?;
+            for cap in re.captures_iter(fields) {
+                let field = cap.name("field").unwrap().as_str();
+                let value = Self::anti_escape(cap.name("value").unwrap().as_str());
+                match field {
+                    #(
+                        stringify!(#fields) => {
+                            let field: #ty = Some(value.parse().unwrap());
+                            result.#fields = field;
+                        }
+                    )*
+                    _ => {}
+                }
+            }
+            Ok(result)
+        }
+    }
+}
+
+fn impl_cq_code(ast: &DeriveInput) -> proc_macro::TokenStream {
+    let name = &ast.ident;
+    let ty = get_type(ast);
+    let fields = get_field(ast);
+    let fn_to_string = impl_to_string(name, &fields);
+    let fn_from_string = impl_from_string(name, &fields, &ty);
     let gen = quote! {
         impl CQCode for #name {
-            fn to_string(&self) -> String {
-                #body
-            }
+            #fn_to_string
+
+            #fn_from_string
         }
     };
     gen.into()
